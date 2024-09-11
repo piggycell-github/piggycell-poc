436551f494a018fe58e6b589b4454320a14dc33b
bsangs <tkdals6805@naver.com>
2024-09-11 13:36:06 +0900

    feat: integrate backend

--- src/point-poc-frontend/src/App.jsx
+++ src/point-poc-frontend/src/App.jsx
@@ -1,28 +1,26 @@
-import { useState } from "react";
 import { point_poc_backend } from "declarations/point-poc-backend";
 
 function App() {
-  const [greeting, setGreeting] = useState("");
-
-  function handleSubmit(event) {
+  function handleMint(event) {
     event.preventDefault();
-    const name = event.target.elements.name.value;
-    point_poc_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
+    const userId = event.target.elements.userId.value;
+    const amount = Number(event.target.elements.amount.value);
+    point_poc_backend.mint(userId, amount).then((result) => {
+      console.log(result);
     });
-    return false;
   }
 
   return (
     <main>
       <br />
       <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
+      <form action="#" onSubmit={handleMint}>
+        <label htmlFor="userId">Enter your name: &nbsp;</label>
+        <input id="userId" alt="Name" type="text" />
+        <label htmlFor="amount">Enter amount: &nbsp;</label>
+        <input id="amount" alt="Amount" type="number" />
+        <button type="submit">Mint</button>
       </form>
-      <section id="greeting">{greeting}</section>
     </main>
   );
 }
