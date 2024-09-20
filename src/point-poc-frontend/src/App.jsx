c4b6826d15ad6928515c4194dd8b53863f6f4d84
bsangs <tkdals6805@naver.com>
2024-09-20 13:27:20 +0900

    feat: init front handle

--- src/point-poc-frontend/src/App.jsx
+++ src/point-poc-frontend/src/App.jsx
@@ -1,6 +1,58 @@
-import { point_poc_backend } from "declarations/point-poc-backend";
+import React, { useState, useEffect } from "react";
+import {
+  createActor,
+  canisterId,
+  point_poc_backend,
+} from "declarations/point-poc-backend";
+import { AuthClient } from "@dfinity/auth-client";
+import { Secp256k1KeyIdentity } from "@dfinity/identity-secp256k1";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [authClient, setAuthClient] = useState(null);
+  const [backendInstance, setBackendInstance] = useState(null);
+  const [principal, setPrincipal] = useState(null);
+
+  useEffect(() => {
+    initAuth();
+  }, []);
+
+  useEffect(() => {
+    if (backendInstance && authClient) {
+      backendInstance.getOwner().then((owner) => {
+        const principal = authClient.getIdentity().getPrincipal();
+        console.log("Authenticated as", principal.toText());
+        console.log("Owner is", owner.toText());
+        setIsAdmin(principal.toText() === owner.toText());
+      });
+    } else {
+      setIsAdmin(false);
+    }
+  }, [authClient, backendInstance]);
+
+  const initAuth = async () => {
+    const client = await AuthClient.create();
+    setAuthClient(client);
+
+    if (await client.isAuthenticated()) {
+      setIsAuthenticated(true);
+      handleAuthenticated(client);
+    } else {
+      setIsAuthenticated(false);
+    }
+  };
+
+  const handleAuthenticated = async (client) => {
+    const identity = client.getIdentity();
+
+    setPrincipal(identity.getPrincipal().toText());
+
+    const actor = createActor(canisterId, { agentOptions: { identity } });
+    setBackendInstance(actor);
+    setIsAuthenticated(true);
+  };
+
   function handleMint(event) {
     event.preventDefault();
     const userId = event.target.elements.userId.value;
